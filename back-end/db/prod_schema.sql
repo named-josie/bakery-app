7916b9b6198946444a8a09c20223bf8094c646b6
named.josie <named.josie@gmail.com>
2022-08-30 21:00:13 -0400

    updated seed images and prod files

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -4,10 +4,33 @@
 
 -- Create table
 CREATE TABLE 
-products (
+breads (
     id serial PRIMARY KEY,
     name TEXT NOT NULL,
     price INTEGER NOT NULL,
     description TEXT NOT NULL,
     image  TEXT NOT NULL,
+    is_favorite BOOLEAN
+);
+
+DROP TABLE IF EXISTS cakes;
+CREATE TABLE 
+cakes (
+    id  SERIAL PRIMARY KEY,
+    name    TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    image TEXT NOT NULL,
+    is_favorite BOOLEAN
+);
+
+DROP TABLE IF EXISTS minis;
+CREATE TABLE 
+minis (
+    id  SERIAL PRIMARY KEY,
+    name    TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    image TEXT NOT NULL,
+    is_favorite BOOLEAN
 );
